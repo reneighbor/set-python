6de741ce1723d1af93d850b76730c5d6c4d036f5
Renee Chu <renee@twilio.com>
2015-09-12 12:01:31 -0700

    constructor and tests

--- _set.py
+++ _set.py
@@ -1,4 +1,11 @@
 class Set:
 
-    def __init__(self):
-        pass
+    def __init__(self, initial_elements = []):
+        self.elements = []
+
+        for element in initial_elements:
+            if element not in self.elements:
+                self.elements.append(element)
+
+
+   
--- tests.py
+++ tests.py
@@ -4,8 +4,21 @@ from _set import Set
 
 class SetTest(unittest.TestCase):
 
-    def test_construct(self):
+    def test_construct_empty(self):
         test_set = Set()
-        print test_set
 
-        assert 1 == 2
+        assert test_set.elements == []
+
+    def test_construct_initial_elements(self):
+        test_set = Set([1, 2, 3])
+
+        # TODO when size() is implemented, assert
+        # size is 3
+        assert test_set.elements == [1, 2, 3]
+
+    def test_construct_initial_elements_duplicates(self):
+        test_set = Set([1, 2, 2])
+
+        # TODO when size() is implemented, assert
+        # size is 2
+        assert test_set.elements == [1, 2]
