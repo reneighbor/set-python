f950590d73257bb2f6bfd33f21976a75a7b72877
Renee Chu <renee@twilio.com>
2015-09-16 22:44:17 -0700

    built out basic functions, converted those testss to use contains()

--- _set_hash.py
+++ _set_hash.py
@@ -7,13 +7,27 @@ class Set:
             if self.contains(element) == False:
                 self.elements[element] = True
 
-
     def add(self, element):
         self.elements[element] = True
 
+    def remove(self, element):
+        if self.elements.get(element):
+            self.elements.pop(element)
+
     def contains(self, element):
-        # if element in self.element.keys() // Inefficient way, 
-        # need to iterate thru whole list
         if self.elements.get(element):
             return True
         return False
+
+    def len(self):
+        return len(self.elements)
+
+    def equals(self, _set):
+        if isinstance(_set, Set) == False:
+            return False
+
+        if self.elements == _set.elements:
+            return True
+        return False
+
+
